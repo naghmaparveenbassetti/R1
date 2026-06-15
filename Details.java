20ce79c41b0a44a4f93a5c8ccf05e128a97998c0
naghmaparveenbassetti <naghma.parveen@bassetti-group.com>
2026-06-15 14:52:06 +0530

    Updated file

--- Details.java
+++ Details.java
@@ -2,30 +2,30 @@ class Student{
     String sName;
     int sId;
     int sRoll;
-    String dept;
-    public void Secondary(String sName, int sId){
-        this.sName=sName;
-        this.sId=sId;
-        System.out.println("The name of the student is: "+this.sName);
-        System.out.println("The id of the student is: "+this.sId);
+    String sDepart;
+    public void Secondary(String name, int id){
+        this.sName=name;
+        this.sId=id;
+        System.out.println("The name of the student is: "+sName);
+        System.out.println("The id of the student is: "+sId);
     }
-    public void HigherSecondary(String sName, int sId,int sRoll){
-        this.sName=sName;
-        this.sId=sId;
-        this.sRoll=sRoll;
-        System.out.println("The name of the student is: "+this.sName);
-        System.out.println("The id of the student is: "+this.sId);
-        System.out.println("The roll number of the student is: "+this.sRoll);
+    public void HigherSecondary(String name, int id,int roll){
+        this.sName=name;
+        this.sId=id;
+        this.sRoll=roll;
+        System.out.println("The name of the student is: "+sName);
+        System.out.println("The id of the student is: "+sId);
+        System.out.println("The roll number of the student is: "+sRoll);
     }
-    public void College(String sName, int sId, int sRoll, String dept){
-        this.sName=sName;
-        this.sId=sId;
-        this.sRoll=sRoll;
-        this.dept=dept;
-        System.out.println("The name of the student is: "+this.sName);
-        System.out.println("The id of the student is: "+this.sId);
-        System.out.println("The roll number of the student is: "+this.sRoll);
-        System.out.println("The department of the student is: "+this.dept);
+    public void College(String name, int id, int roll, String dept){
+        this.sName=name;
+        this.sId=id;
+        this.sRoll=roll;
+        this.sDepart=dept;
+        System.out.println("The name of the student is: "+sName);
+        System.out.println("The id of the student is: "+sId);
+        System.out.println("The roll number of the student is: "+sRoll);
+        System.out.println("The department of the student is: "+sDepart);
     }
     public void TotalStudent(int sCount, int hCount, int cCount){
         int totalCount=(sCount+hCount+cCount);
