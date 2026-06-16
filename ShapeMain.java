6582fd75a7c2c22b6ce63a43fdb6e3effdaf911b
naghmaparveenbassetti <naghma.parveen@bassetti-group.com>
2026-06-16 16:06:21 +0530

    Replace the file for shape

--- ShapeMain.java
+++ ShapeMain.java
@@ -1,9 +1,10 @@
+ //Constructor overloading
  class Shape{
     double length,breadth,width;
     Shape(){
         length=breadth=width=0;
     }
-    Shape (double len,double br){
+    Shape(double len,double br){
         length=len;
         breadth=br;
     }
@@ -12,18 +13,26 @@
         breadth=br;
         width=wd;
     }
-    void Volume(){
-        double vol=(length*breadth*width);
+    void Area(){
         double area=(length*breadth);
         System.out.println("The area of the shape is: "+area);
+    }
+    void Volume(){
+        double vol=(length*breadth*width);
         System.out.println("The volume of the shape is:" +vol);
     }
 }
 public class ShapeMain{
     public static void main(String[] args) {
-     Shape s=new Shape();
+    Shape s=new Shape();
+    s.Area();
+    s.Volume();
     Shape s1=new Shape(40,20);
+    s1.Area();
+    s1.Volume();
     Shape s2=new Shape(60,30,10);
-    }
-   
+    s2.Area();
+    s2.Volume();
+    
+    }  
 }
